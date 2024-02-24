08df78ee784d40cf801add65bd11e2ee276bfde8
Azlan-saja <azlansaja19@gmail.com>
2024-02-24 22:14:53 +0700

    Add Meta Web

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title')</title>
+    <meta name="author" content="Azlansaja">
+    <meta name="description" content="Website Finance Sekolah Online By Azlansaja To YSPDI ROBBANI">
+    <meta name="keywords" content="aplikasi online, aplikasi finance, finance online, aplikasi finance online, finance sekolah online, aplikasi finance sekolah" inertia="keywords">
+    <meta name="robots" content="index,follow" inertia="robots">
+    <meta name="distribution" content="global" inertia="distribution">
+    <meta name="rating" content="general" inertia="rating">
+    <link type="text/plain" rel="author" href="{{ asset('credits.txt') }}" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-title" content="Finance Sekolah Online">
+    <meta name="application-name" content="Aplikasi Finance Sekolah Online">
+    <title>@yield('title') | Finance Sekolah Online</title>
     <link
         rel="shortcut icon"
         type="image/jpg"
